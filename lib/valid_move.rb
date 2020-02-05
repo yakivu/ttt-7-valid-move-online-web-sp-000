f8159fec89156e5248264c6fe3b5052223435848
Adam Walker <adamw207@yahoo.com>
2020-02-05 15:21:53 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,3 +10,34 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?
 end
+
+array[index_number]
+def position_taken
+  board = []
+  index = 0 
+def
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    index = 0
+    expect(valid_move?(board, index)).to be_truthy
+  end
+
+  def
+    board = ["nil ", " ", " ", " ", " ", " ", " ", " ", " "]
+    index = 0
+    expect(valid_move?(board, index)).to be_truthy
+  end
+
+def
+    board = ["nil ", " ", " ", " ", " ", " ", " ", " ", " "]
+    index = 0
+
+    expect(valid_move?(board, index)).to be_falsey
+  end
+
+  def
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    index = 100
+
+    expect(valid_move?(board, index)).to be_falsey
+  end
+end
