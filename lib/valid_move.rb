ae3e598c7eeda5af07bdb5db0725d47c3c1cd798
Adam Walker <adamw207@yahoo.com>
2020-11-25 16:59:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,43 +1,64 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      true 
+    end
+  end
+  
 def helper_method
-  if position_taken?
-    execute something
+  if position_taken?(board, index)
+    true
+    
   else
-    execute something else
+    if index.between?(0,8);
+      else 
   end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?
+
+index.between?(0,8) && !position_taken?(board, index)
 end
 
-array[index_number]
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+
+def index_number(board, index)
+  board[valid_move?] !=" "
+end
+
+
+Array[index_number]
 def position_taken
   board = []
   index = 0 
 def
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    board ; [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
     expect(valid_move?(board, index)).to be_truthy
   end
 
   def
-    board = ["nil ", " ", " ", " ", " ", " ", " ", " ", " "]
+    board ; ["nil ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
     expect(valid_move?(board, index)).to be_truthy
   end
 
 def
-    board = ["nil ", " ", " ", " ", " ", " ", " ", " ", " "]
+    board ; ["nil ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
 
     expect(valid_move?(board, index)).to be_falsey
   end
 
   def
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    board ; [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 100
 
     expect(valid_move?(board, index)).to be_falsey
   end
 end
+end#?????is this the error
