7987338e4d75e1c5ad22b9fb0aac185d814e9cd7
Adam Walker <adamw207@yahoo.com>
2020-11-25 17:08:09 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,43 +1,25 @@
-# code your #valid_move? method here
-def helper_method
-  if position_taken?
-    execute something
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
   else
-    execute something else
-  end
-end
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?
-end
-
-array[index_number]
-def position_taken
-  board = []
-  index = 0 
-def
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 0
-    expect(valid_move?(board, index)).to be_truthy
+    return false
   end
 
-  def
-    board = ["nil ", " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 0
-    expect(valid_move?(board, index)).to be_truthy
-  end
 
-def
-    board = ["nil ", " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 0
-
-    expect(valid_move?(board, index)).to be_falsey
-  end
-
-  def
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 100
-
-    expect(valid_move?(board, index)).to be_falsey
-  end
 end
